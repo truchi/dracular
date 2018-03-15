d5546f2784da459bdbaaa042df3cae468bfb96e9
Romain TRUCHI <romain.truchi.06@gmail.com>
2018-03-15 19:26:56 +0100

    WIP parser

---
+++ node/classes/Parser2.js
@@ -0,0 +1,55 @@
+class Parser {
+  constructor() {
+  }
+
+  parse(string) {
+    const names = [
+      'Background'
+    , 'Current Line'
+    , 'Selection'
+    , 'Foreground'
+    , 'Comment'
+    , 'Cyan'
+    , 'Green'
+    , 'Orange'
+    , 'Pink'
+    , 'Purple'
+    , 'Red'
+    , 'Yellow'
+    ]
+
+    const  sepRe  = '\\s*\\|\\s*'
+    const nameRe  = `(${ names.map(name => name.replace(' ', '\\s')).join('|') })`
+    const  hexRe  = '`#([0-9|a-f]{6})`'
+    const  rgbRe  = '`(\\d{1,3})\\s(\\d{1,3})\\s(\\d{1,3})`'
+    const  hslRe  = '`(\\d{1,3})°\\s(\\d{1,3})%\\s(\\d{1,3})%`'
+    const  fullRe =
+      `${ nameRe }${ sepRe }${ hexRe }${ sepRe }${ rgbRe }${ sepRe }${ hslRe }`
+    const regexp = new RegExp(fullRe, 'i')
+
+    return string
+      .split('\n')
+      .filter(line => names.filter(name => line.startsWith(name)).length)
+      .map(line => {
+        const matches = regexp.exec(line)
+        if (!matches) return
+
+        const name = matches[1]
+        const hex  = matches[2]
+        const rgb  = matches[3]
+        const hsl  = matches[4]
+        return {
+          name: matches[1]
+        , hex : matches[2]
+        , r   : matches[3]
+        , g   : matches[4]
+        , b   : matches[5]
+        , h   : matches[6]
+        , s   : matches[7]
+        , l   : matches[8]
+        }
+      })
+  }
+}
+
+module.exports = Parser
--- node/main.js
+++ node/main.js
@@ -10,18 +10,25 @@ Github
   .authenticate(secret.githubPersonalAccessToken)
   .getRateLimit().then(console.log)
 
-const dracula = new Github({
-  owner: config.draculaOwner
-, repo : config.draculaRepo
-})
+const string = `scheme: "Dracula"\nauthor: "Mike Barkmin (http://github.com/mikebarkmin) based on Dracula Theme (http://github.com/dracula)"\nbase00: "282936" #background\nbase01: "3a3c4e"\nbase02: "4d4f68"\nbase03: "626483"\nbase04: "62d6e8"\nbase05: "e9e9f4" #forground\nbase06: "f1f2f8"\nbase07: "f7f7fb"\nbase08: "ea51b2"\nbase09: "b45bcf"\nbase0A: "00f769"\nbase0B: "ebff87"\nbase0C: "a1efe4"\nbase0D: "62d6e8"\nbase0E: "b45bcf"\nbase0F: "00f769"`
+const parser = new Parser()
+const res = parser
+  .parse(string)
 
-dracula.getREADME()
-  .then(readme => {
-    const parser = new Parser()
+console.log(res);
 
-    const data = parser.parse(readme)
-    console.log(data);
-  })
+// const dracula = new Github({
+//   owner: config.draculaOwner
+// , repo : config.draculaRepo
+// })
+//
+// dracula.getREADME()
+//   .then(readme => {
+//     const parser = new Parser()
+//
+//     const data = parser.parse(readme)
+//     console.log(data);
+//   })
 
 // const atom = new Github({
 //   owner: config.draculaOwner
